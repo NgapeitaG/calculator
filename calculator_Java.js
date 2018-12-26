95501030f2564cd16a66990e1dadcf68aa21c869
Ngapeita Leilua - Gotz <ngapeita@outlook.com>
2018-12-26 18:19:47 +1300

    new java code

--- calculator_Java.js
+++ calculator_Java.js
@@ -1,108 +1,74 @@
 // JavaScript Document
-const calculator = {
-  displayValue: '0',
-  firstOperand: null,
-  waitingForSecondOperand: false,
-  operator: null,
-};
-
-//updates the disabled form that is the display, displayValue keeps at as 0
-function updateDisplay() {
-  const display = document.querySelector('.calc-scn'); //looks for class calc-scn(the disabled form)
-  display.value = calculator.displayValue;
+$(function() {
+  const operators = {
+    "x" : "*",
+    "÷" : "/",
+    "+" : "+",
+    "-" : "-",
   }
-  
-  updateDisplay();
-  
-const keys = document.querySelector('.grid-container'); //looks for grid-container and its children
-keys.addEventListener('click', (event) => {
-const { target } = event;
-if (!target.matches('button')) {
-  return;
-}
 
-if (target.classList.contains('operator')) {
-  handleOperator(target.value);
-      updateDisplay();
-  return;
-}
+  // selectors
+  const $display = $(".calculator__display");
+  const $equalsKey = $(".key--equal");
+  const $expressionInputs = $(".key--operand, .key--operator");
 
-if (target.classList.contains('decimal')) {
-  inputDecimal(target.value);
-      updateDisplay();
-  return;
-}
+  var functionExpression = [];
+  var lastInput = null;
 
-if (target.classList.contains('all-clear')) {
-  resetCalc();
-      updateDisplay();
-  return;
-}
+  // Reset the calculator with AC button
+  $(".clear").click(() => reset(true));
 
-entDig(target.value);
-updateDisplay();
+  $expressionInputs.click((event) => {
+    let currentInput = $(event.target).text();
+    
+    // if the current input is an operator
+    if(Object.keys(operators).includes(currentInput)) {
+      currentInput = operators[currentInput];
+
+      if(lastInput === "operator") {
+        if(functionExpression[functionExpression.length -1] !== currentInput) {
+          // error condition - 2 different operators have been pressed sequentially
+          // reset the functionExpression, display an error
+          updateDisplay("invalid input");
+          reset(false);
+        }
+        return;
+      }
+      lastInput = "operator";
+    }
+    else {
+      lastInput = "operand";
+    }
+
+    console.log(`clicking button! ${currentInput}`);
+    // add the input to the end of our functionExpression array
+    functionExpression.push(currentInput);
+    // build display string by joining the array contents
+    let displayValue = functionExpression.join("");
+    updateDisplay(displayValue);
+  })
+
+  $equalsKey.click((event) => {
+    if(functionExpression.length === 0) return;
+
+    let resultString = functionExpression.join("");
+    console.log(`resultString => ${resultString}`);
+
+    let functionExpressionResult = math.eval(resultString);
+    console.log(`the result is: ${functionExpressionResult}`);
+
+    updateDisplay(functionExpressionResult);
+    reset(false);
+  });
+
+  updateDisplay = (value) => $display.text(value);
+
+  reset = (clearScreenText) => {
+    if(clearScreenText) {
+      $display.text("0");
+      console.log(`display cleared, last functionExpression: ${functionExpression}`);
+    }
+    lastInput = null;
+    functionExpression = [];
+  }
 });
-
-function entDig(digit) {
-  const { displayValue, waitingForSecondOperand } = calculator;
-
-  if (waitingForSecondOperand === true) {
-    calculator.displayValue = digit;
-    calculator.waitingForSecondOperand = false;
-  } else {
-    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
-  }
-
-  console.log(calculator);
-}
-
-function inputDecimal(dot) {
-  // If the `displayValue` does not contain a decimal point
-  if (!calculator.displayValue.includes(dot)) {
-    // Append the decimal point
-    calculator.displayValue += dot;
-  }
-}
-
-//looks for opereator key press then stores first number in firstoperand and operator then waits for seocnd number
-function handleOperator(nextOperator) {
-const { firstOperand, displayValue, operator } = calculator
-const inputValue = parseFloat(displayValue);
-
-if (firstOperand == null) {
-  calculator.firstOperand = inputValue;
-} else if (operator) {
-  const result = equals [operator](firstOperand, inputValue); //calls function equals below
-
-  calculator.displayValue = String(result);
-  calculator.firstOperand = result;
-}
-
-calculator.waitingForSecondOperand = true;
-calculator.operator = nextOperator;
-console.log(calculator);
-}
-
-//looks for operator passed through, works like if/else until it finds the correct opertator, then
-//performs function
-const equals = {
-  '/': (firstOperand, secondOperand) => firstOperand / secondOperand,
-
-  '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
-
-  '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
-
-  '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
-//'%':
-
-  '=': (firstOperand, secondOperand) => secondOperand
-};
-
-//resets calculator obj to defaults  
-function resetCalc() {
-  calculator.displayValue = '0';
-  calculator.firstOperand = null;
-  calculator.waitingForSecondOperand = false;
-  calculator.operator = null;
-  console.log(calculator);
-}
